4b870925a8a10b9fb96d64f2ac148bb6c2e03d4d
Matt <hansonmcoombs@gmail.com>
2024-10-08 09:27:38 +1300

    new stock rate conversion system

--- src/komanawa/simple_farm_model/__init__.py
+++ src/komanawa/simple_farm_model/__init__.py
@@ -4,3 +4,4 @@ A package for simple farm models.
 from komanawa.simple_farm_model.version import __version__
 from komanawa.simple_farm_model.base_simple_farm_model import BaseSimpleFarmModel
 from komanawa.simple_farm_model.simple_dairy_model import SimpleDairyModel, DairyModelWithSCScarcity, default_peak_cow
+from komanawa.simple_farm_model.stock_rate_conversion import calc_full_farm_stock_rate
