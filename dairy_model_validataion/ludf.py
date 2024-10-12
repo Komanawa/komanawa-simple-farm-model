52f4460b79712c57734cdd1d6dffd87489ba9b32
Matt <hansonmcoombs@gmail.com>
2024-10-12 18:25:03 +1300

    start framing LUDF multiyear validation

---
+++ dairy_model_validataion/ludf.py
@@ -0,0 +1,21 @@
+"""
+created matt_dumont 
+on: 10/12/24
+"""
+import pandas as pd
+
+
+def read_data():  # todo
+    raise NotImplementedError
+
+
+def dairy_platform_run():  # todo
+    raise NotImplementedError
+
+
+def full_farm_feed_limited_run():  # todo
+    raise NotImplementedError
+
+
+def raw_full_farm_run():  # todo
+    raise NotImplementedError
